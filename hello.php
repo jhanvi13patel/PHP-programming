5c975801be1f7b193aa938c31c04fffdc59f8a7a
jhanvi13patel <108231214+jhanvi13patel@users.noreply.github.com>
2022-06-27 19:28:28 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+echo "Hello World! <br> Sum = "; 
+
+$x = 5 /* + 15 */ + 5;
+echo $x;
+
+?>
